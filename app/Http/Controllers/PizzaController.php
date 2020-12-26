290c913f747eeaf656bd70807627dbc0b36806fa
Deigan Millikin <dmillikin@fieldsafesolutions.com>
2020-12-26 13:27:08 -0700

    readded 'use App\Models\Pizza;' in PizzaController.php, it is working again

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pizza;
+
+// For some reason this went away, error was said to be on line with ' $pizzas = Pizza::all();'
+
 class PizzaController extends Controller
 {
     public function index()
     {
-        $pizzas = [
-            ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-            ['type' => 'volcano', 'base' => 'garlic crust'],
-            ['type' => 'veg supreme', 'base' => 'thin & crispy'],
-        ];
+
+        $pizzas = Pizza::all();
 
         return view('pizzas', [
             'pizzas' => $pizzas,
